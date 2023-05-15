9eec4e793bf1ac076c4b6f13a5e7f0bf146653b0
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-15 21:01:13 +0530

    Learned  tuple in datatype concept

---
+++ tuple.py
@@ -0,0 +1,23 @@
+a = (1, 2, 3, 4, 5)
+
+print(a)
+print(type(a))
+
+"""
+tuple---():ordered,immutable,allow duplicate:
+
+append()
+insert()
+update()
+remove--discard()
+clear()
+sort()
+slice()
+count ()
+index()
+pop()
+max()
+min()
+"""
+
+
