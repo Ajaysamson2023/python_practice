5f48b00e6efe3c917666373fae7bdf171f544e90
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-15 21:00:00 +0530

    Learned  list in datatype concept

---
+++ list.py
@@ -0,0 +1,26 @@
+# Datatypes concept List:
+
+a=[1,2,3,4,5]
+b=[1,2,3]
+print(a)
+print(type(a))
+"""
+list--[]:ordered,mutable,allow duplicates
+
+append()
+insert()
+update()
+remove--discard()
+clear()
+sort()
+slice()
+count ()
+index()
+pop()
+max()
+min()
+"""
+
+
+
+
