95c69c6c4a324c0bb7963c9a0e8634222acb4d87
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-15 20:58:15 +0530

    Learned  for loop concept

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+for i in range(0,20,2):
+    print(i)
+
+
+print("--------------------range---------------------------")
+
+for i in range(5):
+    a=int(input("enter A :"))
+    b = int(input("enter B :"))
+    print(a+b)
