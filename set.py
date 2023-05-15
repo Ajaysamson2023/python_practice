d076ec6757d8f23fe879ba6591807a333d8ac70e
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-15 21:02:26 +0530

    Learned  set in datatype concept

---
+++ set.py
@@ -0,0 +1,20 @@
+a = {"ram", "sara", "dad"}
+b = {1, 2, 3, 4, 5}
+print(a, b)
+print(type(b))
+print(type(a))
+"""
+set---{}:unordered,immutable,un indexes,no duplicate 
+append()
+insert()
+update()
+remove--discard()
+clear()
+sort()
+slice()
+count ()
+index()
+pop()
+max()
+min()
+"""
